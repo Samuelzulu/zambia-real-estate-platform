fd2124c1bfd035aaedf547948914cea3e9d4aebc
Samuel Zulu <82372551+Samuelzulu@users.noreply.github.com>
2026-06-24 15:04:15 -0400

    feat: authentication routes - register and login with JWT

--- server/index.js
+++ server/index.js
@@ -6,9 +6,12 @@ dotenv.config();
 
 const app = express();
 
+const authRoutes = require('./routes/auth')
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes)
 
 // Test route
 app.get("/", (req, res) => {
